f3015ad75d25665e5ca0f0e699bc21d9d5de01f9
mamadou lamine ba <laminba2003@gmail.com>
2023-02-19 15:48:41 +0000

    improvements

--- src/app/components/layout/navigation.ts
+++ src/app/components/layout/navigation.ts
@@ -1,9 +1,10 @@
 export class MenuItem {
     label: string;
     url: string;
+    icon: string;
 };
 
 export const MENU: MenuItem[] = [
-    { label: "Countries", url: "/countries" },
-    { label: "Persons", url: "/persons" }
+    { label: "Countries", url: "/countries", icon: "fa fa-envelope" },
+    { label: "Persons", url: "/persons", icon: "fa fa-envelope" }
 ];
